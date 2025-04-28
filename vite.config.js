c7fca0d48cacf00a6f5a52e9f15b0ab031a776dd
Cauanz <61758981+Cauanz@users.noreply.github.com>
2025-04-28 11:55:16 -0300

    🔥 ✨ ♻️ Refactor code style and formatting across multiple files
    - Updated package.json for consistent indentation and formatting.
    - Adjusted postcss.config.js for improved readability.
    - Reformatted App.css, Form.css, NavBar.css, and index.css for consistent indentation.
    - Cleaned up main.jsx with uniform import statements and indentation.
    - Refined tailwind.config.js and vite.config.js for better code clarity and structure.

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import mkcert from 'vite-plugin-mkcert';
-import eslintPlugin from 'vite-plugin-eslint'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import mkcert from "vite-plugin-mkcert";
+import eslintPlugin from "vite-plugin-eslint";
 // import fs from 'fs';
 // import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    eslintPlugin({
-      cache: false,
-      include: ['./src/**/*.js', './src/**/*.jsx'],
-      exclude: [],
-    }), 
-    mkcert()
-  ],
-  // server: {
-  //   https: true
-  // }
-  // server: {
-  //   https: {
-  //     key: fs.readFileSync(path.resolve(__dirname, "localhost+1-key.pem")),
-  //     cert: fs.readFileSync(path.resolve(__dirname, "localhost+1.pem")),
-  //   }
-  // }
-})
+	plugins: [
+		react(),
+		eslintPlugin({
+			cache: false,
+			include: ["./src/**/*.js", "./src/**/*.jsx"],
+			exclude: [],
+		}),
+		mkcert(),
+	],
+	// server: {
+	//   https: true
+	// }
+	// server: {
+	//   https: {
+	//     key: fs.readFileSync(path.resolve(__dirname, "localhost+1-key.pem")),
+	//     cert: fs.readFileSync(path.resolve(__dirname, "localhost+1.pem")),
+	//   }
+	// }
+});
